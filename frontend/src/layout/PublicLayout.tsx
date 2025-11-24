6604c643297ba18346d0b9dc466cddfa5bf1db18
Goran Kitic <kiticgoran90@gmail.com>
2025-11-24 18:59:30 +0100

    Responsive design update 👩🏻‍🚀

--- frontend/src/layout/PublicLayout.tsx
+++ frontend/src/layout/PublicLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const PublicLayout = () => {
     return (
         <main className="flex min-h-screen items-center justify-center px-4">
-            <div className="w-full max-w-sm sm:max-w-md">
+            <div className="w-full max-w-sm sm:max-w-lg">
                 <Outlet />
             </div>
         </main>
